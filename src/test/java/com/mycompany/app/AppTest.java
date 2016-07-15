764ecbe47de050cc96bc82c518f71ed8a892adb8
Cemo <cemalettin.koc@gmail.com>
2016-07-15 18:10:27 +0300

    fix

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -2,50 +2,33 @@ package com.mycompany.app;
 
 import java.util.Map;
 import java.util.Properties;
-import junit.framework.Test;
 import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.junit.Test;
 
 /**
  * Unit test for simple App.
  */
+
 public class AppTest
     extends TestCase
 {
     /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
-
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
      * Rigourous Test :-)
      */
+    @Test
     public void testApp()
     {
 
         Properties properties = System.getProperties();
 
         for (Map.Entry<Object, Object> entry : properties.entrySet()) {
-            System.out.println(entry.getKey().toString() + " " + entry.getValue().toString());
+            //System.out.println(entry.getKey().toString() + " " + entry.getValue().toString());
         }
 
         Map<String, String> env = System.getenv();
 
         for (Map.Entry<String , String> entry : env.entrySet()) {
-            System.out.println(entry.getKey() + " " + entry.getValue());
+            //System.out.println(entry.getKey() + " " + entry.getValue());
         }
 
         assertTrue( true );
